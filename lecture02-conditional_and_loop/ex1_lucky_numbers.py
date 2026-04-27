a3393cdd3ad2d0b9875a70813d5383d4d35313a6
Nguyen Hoang Minh Huy <nguyenhoangminhhuy23022009@gmail.com>
2026-04-27 21:17:44 +0700

    python exercise 1 lecture 02

---
+++ lecture02-conditional_and_loop/ex1_lucky_numbers.py
@@ -0,0 +1,11 @@
+a,b=map(int,input().split())
+x=int(input())
+if x%a==0 and x%b==0:
+  print('Both')
+else:
+  if x%a==0:
+    print('Upan')
+  elif x%b==0:
+    print('Ipan')
+  else:
+    print('No')
